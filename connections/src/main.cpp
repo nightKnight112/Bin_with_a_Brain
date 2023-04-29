e1eafe7991df837785c017a3a4bf70133e6e6bb0
PM-o-428 <pritammukherjee556@gmail.com>
2023-04-29 21:18:52 +0530

    Complete Wifi connection code with WifiMulti

--- connections/src/main.cpp
+++ connections/src/main.cpp
@@ -1,9 +1,62 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiMulti.h>
+#include <PubSubClient.h>
+
+WiFiMulti wifiMulti;
+// WiFi connect timeout per AP. Increase when connecting takes longer.
+const uint32_t connectTimeoutMs = 5000;
+
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(10);
+  WiFi.mode(WIFI_STA);
+  wifiMulti.addAP("Pritam_2.4GHz","Micronet-0-");
+  wifiMulti.addAP("A1_Plus","pritampritam");
+
+  // WiFi.scanNetworks will return the number of networks found
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+  if (n == 0) {
+      Serial.println("no networks found");
+  } 
+  else {
+    Serial.print(n);
+    Serial.println(" networks found");
+    for (int i = 0; i < n; ++i) {
+      // Print SSID and RSSI for each network found
+      Serial.print(i + 1);
+      Serial.print(": ");
+      Serial.print(WiFi.SSID(i));
+      Serial.print(" (");
+      Serial.print(WiFi.RSSI(i));
+      Serial.print(")");
+      Serial.println((WiFi.encryptionType(i) == WIFI_AUTH_OPEN)?" ":"*");
+      delay(10);
+    }
+  }
+
+  // Connect to Wi-Fi using wifiMulti (connects to the SSID with strongest connection)
+  Serial.println("Connecting Wifi...");
+  if(wifiMulti.run() == WL_CONNECTED) {
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  //if the connection to the stongest hotstop is lost, it will connect to the next network on the list
+  if (wifiMulti.run(connectTimeoutMs) == WL_CONNECTED) {
+    Serial.print("WiFi connected: ");
+    Serial.print(WiFi.SSID());
+    Serial.print(" ");
+    Serial.println(WiFi.RSSI());
+  }
+  else {
+    Serial.println("WiFi not connected!");
+  }
+  delay(1000);
 }
