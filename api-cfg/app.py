39d6cc8c5e37e3235ed88b34c5fcd03636424fb6
Archisman Das <76892920+nightKnight112@users.noreply.github.com>
2023-05-07 21:50:26 +0530

    Added mqtt support and updated definitions

--- api-cfg/app.py
+++ api-cfg/app.py
@@ -1,14 +1,28 @@
 import streamlit as st  # localhost api
 import tensorflow as tf
-from tensorflow.keras.models import load_model
-from tensorflow.keras.preprocessing.image import save_img
+from keras.utils import save_img
+from tensorflow import keras
 from PIL import Image
 from fpdf import FPDF
-import random
 import webbrowser
+import random
+import requests
+import geocoder
+import folium
+from streamlit_folium import st_folium
+import paho.mqtt.client as mqtt_client
+
+
+# mqtt routes: Detection Biodegradable/Open and Detection Non-Biodegradable/Open
+
 
 MODELSPATH = 'bin_witha_brain_modelf.h5'
 DATAPATH = 'waste1.jpg'  # sample image
+mqtt_Broker = ""  # localhost ip(use pc mainframe ip)
+client = mqtt_client.Client("Mainframe") # Client name Mainframe
+# client.connect(mqtt_Broker) # uncomment after setting mqtt_Broker
+topic_1 = "Detection Biodegradable"  # topic for publishing if biodegradable
+topic_2 = "Detection Non-Biodegradable"  # topic for publishing if non-biodegradable
 
 i = 0
 
@@ -37,7 +51,7 @@ def funcs(img_pth, label_p, token_and_extras):
     # add another cell
     pdf.cell(200, 10, txt=token_and_extras, ln=2, align='C')
 
-    #pdf name
+    # pdf name
     n = pdf_name_gen(i)
 
     # save the pdf with name .pdf
@@ -46,26 +60,28 @@ def funcs(img_pth, label_p, token_and_extras):
     return n
 
 
-@st.cache
+@st.cache_data
 def load_data():
     img = Image.open(DATAPATH)
     return img
 
 
 def load_models():
-    model = load_model(MODELSPATH, compile=False)
+    model = tf.keras.saving.load_model(MODELSPATH, compile=False)
     return model
 
 
-st.title('Bin with a Brain')  # project title
-st.header('Waste Classification Application based on CNN(software implementation)')
-st.text('This application will let you verify if your waste may be biodegradable or not')
+st.title('Bins with a Brain')  # project title
+st.header('A Waste Management Application for the Future!!!')
+st.text('A Web Application for smart and efficient Waste Management')
 st.markdown('To see prediction one can use a sample data(already provided) or upload their own image.')
 
 st.sidebar.title('Select options:')
-page = st.sidebar.selectbox("Choose a page:", ["Sample Data", "Upload an Image", "Use Camera to Upload Image"])
+page = st.sidebar.selectbox("Choose a page:", ["Sample Data to Test App", "Upload an Image to Report Wastes",
+                                               "Use Camera to Upload Image and Report Wastes",
+                                               "Nearby Reporting point location"])
 
-if page == "Sample Data":
+if page == "Sample Data to Test App":
     st.header("Sample prediction for testing if the model works")
     if st.checkbox('Sample Data exploration'):
         st.info("Sample image:")
@@ -82,12 +98,15 @@ if page == "Sample Data":
             st.write(result)
             if result[0][0] > 0.65:
                 prediction = 'Recyclable/Non-Biodegradable Waste'
+                client.publish(topic_2, "Open")
+
             else:
                 prediction = 'Organic/Biodegradable Waste'
+                client.publish(topic_1, "Open")
             st.write(prediction)
             st.success("It is prediction for sample image and the app is working")
 
-elif page == "Upload an Image":
+elif page == "Upload an Image to Report Wastes":
     st.header("Please upload a photo of the lying wastes to report")
     uploaded_file = st.file_uploader("Choose your image", type=["jpg", "png"])
     if uploaded_file is not None:
@@ -124,7 +143,7 @@ elif page == "Upload an Image":
                                file_name='report.pdf',
                                help='click here to download the generated report for manual submission')
 
-elif page == "Use Camera to Upload Image":
+elif page == "Use Camera to Upload Image and Report Wastes":
     st.header("Your prediction of waste")
     image = st.camera_input("Take a picture")
 
@@ -144,12 +163,14 @@ elif page == "Use Camera to Upload Image":
             st.write(result)
             if result[0][0] > 0.65:  # based on confusion matrix[0][0]
                 prediction = 'Recyclable/Non-Biodegradable Waste'
+                client.publish(topic_2, "Open")
                 token = random.randint(100000, 99999999999)
                 token = str(token)
                 token = 'Token number is:' + str(token)
                 filename = funcs('default.jpg', prediction, token)
             else:
                 prediction = 'Organic/Biodegradable Waste'
+                client.publish(topic_1, "Open")
                 token = random.randint(100000, 99999999999)
                 token = str(token)
                 token = 'Token number is:' + str(token)
@@ -162,3 +183,14 @@ elif page == "Use Camera to Upload Image":
             st.download_button("Download the Generated Report", filename, key='download',
                                file_name='report.pdf',
                                help='click here to download the generated report for manual submission')
+
+elif page == "Nearby Reporting point location":
+
+    st.header("Your Nearest Report Center is at: ")
+
+    # g = geocoder.ip('me')
+    # lcn = g.latlng # dynamic ip generation
+    map_ac = folium.folium.Map(location=[22.977026925839883, 88.44656142617595], zoom_start=16)  # hcded to kly_munc
+    folium.Marker([22.977026925839883, 88.44656142617595], popup="Waste disposal", tooltip="central disposal").add_to(
+        map_ac)
+    st_dta = st_folium(map_ac, width=725)
